723598b5acdab4e0fdb873049a6d662c2d567c14
MarcinAman <marcin.aman@gmail.com>
2018-12-06 19:13:07 +0100

    cleanup

--- HarrisCornerDetection.py
+++
@@ -1,4 +0,0 @@
-# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_features_harris/py_features_harris.html#harris-corners
-class HarrisCornerDetection:
-    def detect(self, frame):
-        return frame

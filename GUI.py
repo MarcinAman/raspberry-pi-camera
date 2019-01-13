c807553319c449d1b773ccba64d4ccc722c7049a
MarcinAman <marcin.aman@gmail.com>
2019-01-13 19:03:24 +0100

    .

--- GUI.py
+++ GUI.py
@@ -1,25 +1,21 @@
 import tkinter as tk
-from PIL import ImageTk, Image
+from threading import Thread
+from VideoRecorder import VideoRecorder
+import cv2
 
 
-def process_image(processing_object):
-    from VideoRecorder import VideoRecorder
-    video_recorder = VideoRecorder()
+def process_image(processing_object, video_recorder):
+    print('params:')
+    print(processing_object.__class__.__name__)
+    print(video_recorder.__class__.__name__)
 
     while True:
-        frame = next(video_recorder.get_frame())
+        frame = next(get_frame(video_recorder))
         cv2.imshow('frame', processing_object.detect(frame))
 
-        if cv2.waitKey(1) % 0xFF == ord('q'):
-            break
 
-    video_recorder.stop()
-
-
-def get_frame():
-    from VideoRecorder import VideoRecorder
-    video_recorder = VideoRecorder()
-
+def get_frame(video_recorder):
+    print('works')
     while True:
         frame = next(video_recorder.get_frame())
         yield frame
@@ -30,17 +26,19 @@ class Application(tk.Frame):
         self.root = tk.Tk()
         super().__init__(self.root)
         self.pack()
-        self.root.geometry("720x500")
+        self.root.geometry("300x250")
         self.processing_object = processing_object
+        self.video_recorder = VideoRecorder()
+        # self.processing_thread = Thread(target=process_image, args=(self.processing_object, self.video_recorder))
 
     def init_buttons(self, button_with_rendering_object):
         for i, value in enumerate(button_with_rendering_object):
             label, processing = value
             self.generate_button(label=label, command=lambda: self.set_processing_object(processing), row=i)
 
-    def render_image(self, frame):
-        img = ImageTk.PhotoImage(frame)
-        panel = tk.Label(self.root, image=img)
+    def init_displaying_thread(self):
+        print('thresd started')
+        # self.processing_thread.start()
 
     def generate_button(self, label, command, row):
         tk.Button(self, text=label, width=25, height=1, command=command).grid(row=row, column=0)
@@ -48,3 +46,9 @@ class Application(tk.Frame):
     def set_processing_object(self, processing_object):
         print('value changed to:' + str(processing_object.__class__.__name__))
         self.processing_object = processing_object
+
+    def destroy(self):
+        print('destroy')
+        self.video_recorder.stop()
+        # self.processing_thread.join()
+        super(Application, self).destroy()
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sys
-
-import cv2
-
 from SobelDetection import SobelDetection
-from VideoRecorder import VideoRecorder
 from FaceDetection import FaceDetection
 from BackgroundFiltering import BackgroundFiltering
 from NoneFilter import NoneFilter
@@ -12,51 +7,11 @@ from LaplacianOperator import LaplacianOperator
 from FaceAndEyesDetection import FaceAndEyesDetection
 import GUI
 
-
-def process_image(processing_object):
-    video_recorder = VideoRecorder()
-
-    while True:
-        frame = next(video_recorder.get_frame())
-        print(str(frame.__class__.__name__))
-        cv2.imshow('frame', processing_object.detect(frame))
-
-        if cv2.waitKey(1) % 0xFF == ord('q'):
-            break
-
-    video_recorder.stop()
-
-
-def parse_command_line(argv):
-    mode = argv[0].strip()
-
-    print('Initialized with mode: ' + mode)
-
-    if mode == 'face-detection':
-        return FaceDetection()
-    if mode == 'sobel':
-        return SobelDetection()
-    if mode == 'background-filter':
-        return BackgroundFiltering()
-    if mode == 'smoothing':
-        return Smoothing()
-    if mode == 'none':
-        return NoneFilter()
-    if mode == 'laplacian':
-        return LaplacianOperator()
-    if mode == 'eyes-face-detection':
-        return FaceAndEyesDetection()
-
-    raise RuntimeError('Mode not recognised!')
-
-
 if __name__ == "__main__":
     app = GUI.Application(NoneFilter())
     buttons = [('Face detection', FaceDetection()), ('Face and eyes detection', FaceAndEyesDetection()),
                ('Sobel', SobelDetection()), ('Smoothing', Smoothing()), ('Laplacian', LaplacianOperator()),
                ('Background filter', BackgroundFiltering())]
     app.init_buttons(button_with_rendering_object=buttons)
-    cv2.imwrite()
-    app.render_image(next(GUI.get_frame()))
+    app.init_displaying_thread()
     app.mainloop()
-    # process_image(parse_command_line(sys.argv[1:]))
